da150b0aec1baf8a6bf4007648489c7ec5887e2b
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2017-11-27 01:28:12 -0400

    Arreglada torre, otra vez

--- prolog files/torre.pl
+++ prolog files/torre.pl
@@ -3,23 +3,23 @@ dentroTableroTorre(CL, I, J) :- I >= 0, I =< 7, J >= 0, J =< 7, \+pieza(CL, I, J
 diferenteTorre(X,Y,X1,Y1):- X \= X1;Y \= Y1.
 
 torre(_, _, AX, MX, AY, MY) :- AX = MX, AY = MY.
-torre(CL, C, AX, MX, AY, MY) :- 
+torre(CL, C, AX, MX, AY, MY) :-
     diferenteTorre(AX, AY, MX, MY),
-    C = 1, X is AX + 1, Y is AY, 
-    dentroTableroTorre(CL, X, Y), 
+    C = 1, X is AX + 1, Y is AY,
+    dentroTableroTorre(CL, X, Y),
     torre(CL, C, X, MX, Y, MY).
-torre(CL, C, AX, MX, AY, MY) :- 
+torre(CL, C, AX, MX, AY, MY) :-
     diferenteTorre(AX, AY, MX, MY),
-    C = 2, X is AX + 1, Y is AY, 
-    dentroTableroTorre(CL, X, Y), 
+    C = 2, X is AX, Y is AY + 1,
+    dentroTableroTorre(CL, X, Y),
     torre(CL, C, X, MX, Y, MY).
-torre(CL, C, AX, MX, AY, MY) :- 
+torre(CL, C, AX, MX, AY, MY) :-
     diferenteTorre(AX, AY, MX, MY),
-    C = 3, X is AX + 1, Y is AY, 
-    dentroTableroTorre(CL, X, Y), 
+    C = 3, X is AX - 1, Y is AY,
+    dentroTableroTorre(CL, X, Y),
     torre(CL, C, X, MX, Y, MY).
-torre(CL, C, AX, MX, AY, MY) :- 
+torre(CL, C, AX, MX, AY, MY) :-
     diferenteTorre(AX, AY, MX, MY),
-    C = 4, X is AX + 1, Y is AY, 
-    dentroTableroTorre(CL, X, Y), 
+    C = 4, X is AX, Y is AY - 1,
+    dentroTableroTorre(CL, X, Y),
     torre(CL, C, X, MX, Y, MY).
