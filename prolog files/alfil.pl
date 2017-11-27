14737ccf20fd8c87798bad1637771c1d8a477893
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2017-11-27 00:32:33 -0400

    Verificando que la posición sea diferene en alfil y torre

--- prolog files/alfil.pl
+++ prolog files/alfil.pl
@@ -1,11 +1,25 @@
 dentroTableroAlfil(CL, I, J) :- I >= 0, I =< 7, J >= 0, J =< 7, \+pieza(CL, I, J).
 
+diferenteAlfil(X,Y,X1,Y1):- X \= X1;Y \= Y1.
+
 alfil(_, _, AX, MX, AY, MY) :- AX = MX, AY = MY.
-alfil(CL, C, AX, MX, AY, MY) :- C = 1, X is AX - 1, Y is AY - 1, 
-    dentroTableroAlfil(CL, X, Y), alfil(CL, C, X, MX, Y, MY).
-alfil(CL, C, AX, MX, AY, MY) :- C = 2, X is AX + 1, Y is AY + 1, 
-    dentroTableroAlfil(CL, X, Y), alfil(CL, C, X, MX, Y, MY).
-alfil(CL, C, AX, MX, AY, MY) :- C = 3, X is AX - 1, Y is AY + 1, 
-    dentroTableroAlfil(CL, X, Y), alfil(CL, C, X, MX, Y, MY).
-alfil(CL, C, AX, MX, AY, MY) :- C = 4, X is AX + 1, Y is AY - 1, 
-    dentroTableroAlfil(CL, X, Y), alfil(CL, C, X, MX, Y, MY).
+alfil(CL, C, AX, MX, AY, MY) :- 
+    diferenteAlfil(AX, AY, MX, MY), 
+    C = 1, X is AX - 1, Y is AY - 1, 
+    dentroTableroAlfil(CL, X, Y), 
+    alfil(CL, C, X, MX, Y, MY).
+alfil(CL, C, AX, MX, AY, MY) :- 
+    diferenteAlfil(AX, AY, MX, MY),
+    C = 2, X is AX + 1, Y is AY + 1, 
+    dentroTableroAlfil(CL, X, Y), 
+    alfil(CL, C, X, MX, Y, MY).
+alfil(CL, C, AX, MX, AY, MY) :- 
+    diferenteAlfil(AX, AY, MX, MY),
+    C = 3, X is AX - 1, Y is AY + 1, 
+    dentroTableroAlfil(CL, X, Y), 
+    alfil(CL, C, X, MX, Y, MY).
+alfil(CL, C, AX, MX, AY, MY) :- 
+    diferenteAlfil(AX, AY, MX, MY),
+    C = 4, X is AX + 1, Y is AY - 1, 
+    dentroTableroAlfil(CL, X, Y), 
+    alfil(CL, C, X, MX, Y, MY).
